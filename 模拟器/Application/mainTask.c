7b10ca0418578a6605d343d577917ca30f21ec0c
yunshu-drwells <2582050678@qq.com>
2024-10-21 05:49:41 +0800

    添加GUI实现过程中GUISimulationI模拟器实现界面的项目代码

---
+++ 模拟器/Application/mainTask.c
@@ -0,0 +1,29 @@
+#include "dialog.h"//包含window对话框 头文件
+
+extern GUI_CONST_STORAGE GUI_FONT GUI_Fontfont;
+
+void MainTask(void)
+
+{
+
+	GUI_Init();                     //初始化emWin/ucGUI
+    GUI_UC_SetEncodeUTF8();         //使能utf-8方案
+
+    //GUI_SetFont(&GUI_Fontfont);  // 设置字体
+    //GUI_DispString("主灯");
+    //GUI_SetFont(GUI_FONT_32_ASCII);  // 设置字体
+    //GUI_DispString("233");
+
+	//CreateWindow();                 //创建窗体,父窗体是桌面背景
+	CreateWindowMain();
+	//CreateTemperature();  //测试Temperature子窗口
+	//CreateHumidity();  //测试Humidity子窗口
+	//CreateBrightnesss();  //测试Brightnesss子窗口
+	//CreateAlarm();  //测试Alarm子窗口
+	//CreateFan();  //测试Fan子窗口
+	//CreateLightingMaster();  //测试LightingMaster子窗口
+	//CreateSpotlight();  //测试Spotlight子窗口
+    //CreateTurnOff();  //测试TurnOff子窗口
+	while(1) {GUI_Delay(20);}       //调用GUI_Delay函数延时20MS(最终目的是调用GUI_Exec()函数)
+
+}
